e5c15194b704e64bcd4fa9a7dd2a8cdc261c3f45
Rajat9927 <Rajat9927kumar@gmail.com>
2023-08-25 23:21:37 +0530

    podcast

--- src/pages/Podcasts.js
+++ src/pages/Podcasts.js
@@ -6,7 +6,26 @@ import { setPodcasts } from '../slices/podcastSlice';
 import { db } from '../firebase';
 
 function PodcastsPage() {
-    
+    const dispatch = useDispatch();
+    const podcasts = useSelector((state) =>state.podcasts.podcasts);
+    useEffect(() => {
+            const unsubscribe = onSnapshot(
+              query(collection(db, "podcasts")),
+              (querySnapshot) => {
+                const podcastsData = [];
+                querySnapshot.forEach((doc) =>{
+                    podcastsData.push({d:doc.id, ...doc.data()});
+                });
+                dispatch(setPodcasts(podcastsData));
+              },
+              (error) => {
+                console.log(error);
+              }
+            );
+            return () => {
+              unsubscribe();
+            }; 
+        },[dispatch]);
       
 
     return (
@@ -15,7 +34,7 @@ function PodcastsPage() {
             <div className='input-wrapper'>
 
                 <h1 style={{marginTop: '1rem'}}>Discover Podcast</h1>
-                
+                {podcasts.length>0?<p>Podcasts Found</p>:<p>No Podcast Found</p>}
             </div>
         </div>
     )
